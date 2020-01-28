3bd4b2fed6059f485c3e0cf4eeb296ce58a45122
linkbaghira <39954496+linkbaghira@users.noreply.github.com>
2020-01-28 23:28:42 +0700

    Update Main.java

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -3,7 +3,7 @@ package titactoe;
 //This code simple output game field
 public class Main{
 	public static void main(String[] args){
-		System.out.println("X O X");
+	System.out.println("X O X");
         System.out.println("O X O");
         System.out.println("X X O");
 	}
