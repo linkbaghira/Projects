f366855d72cab17379085be02d6bee2ec14fb4c8
linkbaghira <39954496+linkbaghira@users.noreply.github.com>
2020-01-28 23:39:42 +0700

    Updating game logic

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -1,10 +1,16 @@
-package titactoe;
+ppackage tictactoe;
+import java.util.Scanner;
 
-//This code simple output game field
-public class Main{
-	public static void main(String[] args){
-	System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
-	}
+//This code allow user enter final game field Tic Tac Toe
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scan = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        char[] cells = scan.next().toCharArray();
+        System.out.println("---------");
+        System.out.println("| "+cells[0] + " " + cells[1] + " " + cells[2] + " |");
+        System.out.println("| "+cells[3] + " " + cells[4] + " " + cells[5] + " |");
+        System.out.println("| "+cells[6] + " " + cells[7] + " " + cells[8] + " |");
+        System.out.println("---------");
+    }
 }
